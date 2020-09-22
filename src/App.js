e810d5981990434ea128776d1dd5598c607c2a69
JennPride <Jennpride08@gmail.com>
2020-09-21 23:18:43 -0400

    Slowly working through

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      fileUpload: null,
+      error: null,
+      colorCount: 10,
+      downloadLink: null,
+    };
+  }
+
+  updateCount(colorCount) {
+    colorCount = parseInt(colorCount);
+    if (typeof colorCount === "number") {
+      this.setState({colorCount});
+    }
+  }
+
+  handleFileUpload(event) {
+    const fileUpload = event.target.files[0];
+    this.setState({fileUpload});
+  }
+
+  handleFileSubmit() {
+    const {fileUpload, colorCount} = this.state;
+    const formData = new FormData();
+    formData.append(
+        "fileUpload",
+        fileUpload,
+    );
+    formData.append(
+        "numColors",
+        colorCount
+    );
+
+    this.setState({loading: true});
+    axios.post('http://localhost:3000/submitImage', formData).then((result) => {
+      const {error, newImage} = result.data;
+      if (error) {
+        this.setState({error});
+      } else {
+        this.setState({downloadLink: newImage});
+      }
+    });
+  }
+
+  render() {
+
+    const {colorCount, downloadLink} = this.state;
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1> Colourfill </h1>
+            <div className="App-Container">
+              <h1> Please upload a PNG and select how many colors you would like!</h1>
+              <input value={colorCount} type="number" min="1" max="25" onChange={(e) => {this.updateCount(e.target.value)}}/>
+              <input type="file" id="originalFile" accept="image/png" onChange={(e) => {this.handleFileUpload(e)}} />
+              <button onClick={()=>this.handleFileSubmit()}> Create </button>
+              {
+                downloadLink && (
+                    <a href={downloadLink} download={downloadLink}>
+                      <button>Download</button>
+                    </a>
+                )
+              }
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;

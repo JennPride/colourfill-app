4cc3d980373dc90eb4a846784df55f4ad9fc668f
JennPride <Jennpride08@gmail.com>
2020-09-25 16:16:41 -0400

    Cleaning up css, ACTUALLY downloading file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import loadingImg from './rings.svg';
 
 class App extends React.Component {
 
@@ -11,7 +12,6 @@ class App extends React.Component {
       fileUpload: null,
       error: null,
       colorCount: 10,
-      downloadLink: null,
     };
   }
 
@@ -40,37 +40,65 @@ class App extends React.Component {
     );
 
     this.setState({loading: true});
-    axios.post('http://localhost:3000/submitImage', formData).then((result) => {
-      const {error, newImage} = result.data;
+    axios.post('http://localhost:3000/submitImage', formData, {responseType: 'blob'}).then((result) => {
+      const {error} = result.data;
+      this.setState({loading: false, fileUpload: null});
       if (error) {
         this.setState({error});
       } else {
-        this.setState({downloadLink: newImage});
+        const {fileUpload, colorCount} = this.state;
+        let data = new Blob([result.data]);
+        let link = document.createElement('a');
+        link.href = window.URL.createObjectURL(data);
+        link.download = `${colorCount}-${fileUpload.name}`;
+        document.body.appendChild(link);
+        link.click();
       }
     });
   }
 
   render() {
 
-    const {colorCount, downloadLink} = this.state;
+    const {colorCount, fileUpload, loading} = this.state;
 
     return (
         <div className="App">
           <header className="App-header">
-            <h1> Colourfill </h1>
-            <div className="App-Container">
-              <h1> Please upload a PNG and select how many colors you would like!</h1>
-              <input value={colorCount} type="number" min="1" max="25" onChange={(e) => {this.updateCount(e.target.value)}}/>
-              <input type="file" id="originalFile" accept="image/png" onChange={(e) => {this.handleFileUpload(e)}} />
-              <button onClick={()=>this.handleFileSubmit()}> Create </button>
-              {
-                downloadLink && (
-                    <a href={downloadLink} download={downloadLink}>
-                      <button>Download</button>
-                    </a>
-                )
+            <h1> Colorfill </h1>
+            {loading ?
+            <div className="App-loader">
+              <img src={loadingImg} width="150px"/>
+            </div>
+              :
+            <div className="App-container">
+              <h2> Please upload a PNG and select how many colors you would like!</h2>
+              <p> Our program will recolor your image to use only the most dominant colors - creating a unique stylized picture.</p>
+              <div className="App-inputs">
+                <div className="App-input">
+                  <label htmlFor="colorCount"> Number of Colors: </label>
+                  <input value={colorCount} id="colorCount" type="number" min="1" max="25" onChange={(e) => {
+                    this.updateCount(e.target.value)
+                  }}/>
+                </div>
+                <div className="App-input">
+                  <label className="App-input-file">
+                    <input type="file" id="originalFile" accept="image/png" onChange={(e) => {
+                      this.handleFileUpload(e)
+                    }}/>
+                    Upload PNG
+                  </label>
+                  {!fileUpload ?
+                    <p>No File Selected</p>
+                    :
+                    <p>{fileUpload.name}</p>
+                  }
+                </div>
+              </div>
+              {fileUpload &&
+                <button onClick={()=>this.handleFileSubmit()}> Create </button>
               }
             </div>
+            }
           </header>
         </div>
     );

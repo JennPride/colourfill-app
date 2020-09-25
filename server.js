bc6af15713ee2ad2c3f5843084d00d2b107e0556
JennPride <Jennpride08@gmail.com>
2020-09-25 17:44:06 -0400

    Editing server file and removing reference to react app

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+});
